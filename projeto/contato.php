9dd322dee57bbbf4fc6e6b468a30096665dab477
FilipeQueiroz96 <126695392+FilipeQueiroz96@users.noreply.github.com>
2024-06-10 15:07:06 -0300

    formulario

---
+++ projeto/contato.php
@@ -0,0 +1,23 @@
+<?php
+
+    $nome = addslashes($_POST{'nome'});
+    $telefone = addslashes($_POST{'tel'});
+    $mensagem = addslashes($_POST{'texto'});
+
+
+    $para = "filipequeiroz196@gmail.com";
+    $assunto = "Contato PQFA";
+
+    $corpo = "Nome: ".$nome."\n"."Telefone: ".$tel."\n"."Mensagem: ".texto;
+
+    $cabeca = "From: filipequeiroz196@hotmail.com";
+    
+    if(mail($para,$assunto,$corpo,$cabeca)){
+        echo("E-mail enviado com sucesso!");
+    }
+    else{
+        echo("Houve um erro ao enviar o e-mail");
+    }
+    
+    
+    ?>
