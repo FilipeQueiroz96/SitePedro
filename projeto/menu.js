6f674ae236c6f558fc15242534890a18bcd9fcc6
FilipeQueiroz96 <126695392+FilipeQueiroz96@users.noreply.github.com>
2024-06-12 15:18:51 -0300

    menu mobile

--- projeto/menu.js
+++ projeto/menu.js
@@ -1,6 +1,8 @@
-let botaomenu = document.getElementById('botao-menu')
+/* let botaomenu = document.getElementById('botao-menu')
 let menu = document.getElementById('menu-mobile')
 
 botaomenu.addEventListener('click', ()=> {
     menu.classList.add('abrir-menu')
-})
+}) */
+
+    
